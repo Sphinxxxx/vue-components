87f679c7df607079d28eb7e3e8d36f8e2bbaac6f
Andreas Borgen <coding@enthuses.me>
2024-02-12 17:12:09 +0100

    slider.validator

--- vue-inputs.js
+++ vue-inputs.js
@@ -1,20 +1,41 @@
 Vue.component('slider', {
     template: `
-        <label class="slider">
-            <span>{{ caption }}</span>
-            <input :value="val" type="range" v-bind="attr" @input="handleInput">
-            <output>{{ out }}</output>
+        <label class="slider" :class="classes">
+            <span class="caption">{{ caption }}</span>
+            <input :value="proxy" type="range" v-bind="attr" @input="handleInput" />
+            <input :value="proxy" type="number" :step="attr && attr.step" @change="handleInput" />
+            <span class="message" v-show="msg">{{ msg }}</span>
         </label>
     `,
-    props: ['caption', 'val', 'attr'],
+    props: ['caption', 'val', 'attr', 'validator'],
     data() {
-        return { };
+        return {
+            forceUpdateHack: 0,
+            error: null,
+        };
     },
     computed: {
-        out() {
-            const step = this.attr?.step || 1;
-            return (step < 1) ? this.val.toFixed(1) : this.val;
+        proxy: {
+            get() {
+                if (this.forceUpdateHack < 0) { return 58008; }
+                //console.log('get', this.val);
+                return this.val;
+            },
+        },
+        classes() {
+            const co = this.error && this.error.coerced;
+            return {
+                error: this.error,
+                coerced: (co || (co === 0)),
+            };
+        },
+        msg() {
+            return this.error?.message;
         }
+        //out() {
+        //    const step = this.attr?.step || 1;
+        //    return (step < 1) ? this.val.toFixed(1) : this.val;
+        //},
     },
     model: {
         prop: 'val',
@@ -22,8 +43,50 @@ Vue.component('slider', {
     },
     methods: {
         handleInput(e) {
-            const v = Number(e.target.value);
-            this.$emit('do_it', v);
-        }
+            const str = e.target.value;
+            //console.log('in', JSON.stringify(str));
+            let num;
+            if(str) {
+                num = Number(str);
+            }
+            //https://github.com/vuejs/vue/issues/4742
+            else {
+                const def = this.default;
+                if (def || (def === 0)) {
+                    num = def;
+                }
+                else {
+                    num = null;
+                }
+            }
+            this.update(num);
+        },
+        update(newVal) {
+            const err = this.validator && this.validator(newVal);
+            this.error = err;
+
+            if(err) {
+                //If there is a validation error we have three options, depending on `err.coerced`:
+                //  #1: Use the value anyway, but display an error (no err.coerced)
+                //  #2: Correct the value into something usable (err.coerced !== newVal),
+                //  #3: Discard the value and display an error (err.coerced === newVal)
+                const co = err.coerced;
+                if(co || (co === 0)) {
+                    if(co !== newVal) {
+                        // #2
+                        newVal = co;
+                        //By Vue reactivity, this forces the input fields to revert to the coerced value:
+                        this.forceUpdateHack++;
+                    }
+                    else {
+                        // #3
+                        //Remove the `coerced` property so our CSS classes update correctly:
+                        err.coerced = undefined;
+                        return;
+                    }
+                }
+            }
+            this.$emit('do_it', newVal);
+        },
     },
 });
